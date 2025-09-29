22e77c51e620c6e395e0f5bd0588e62e30510500
Bosbacl_Bobur <bosbacl713@gmail.com>
2025-09-29 14:51:11 +0500

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import TradingPanel from "./components/TradingPanel";
-
 function App() {
   return (
-    <div>
-      <TradingPanel />
+    <div className="h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind ishlayapti! 🚀
+      </h1>
     </div>
   );
 }
